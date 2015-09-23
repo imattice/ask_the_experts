8a79ad17125c7c3b7e541e0f4a3551fafeb3c197
Jason Bethel & Ike Mattice <student@epicodus.com>
2015-09-23 15:50:55 -0700

    add functionality to display answers.

--- controllers/AnswersController.js
+++ controllers/AnswersController.js
@@ -1,7 +1,8 @@
 askTheExperts.controller('AnswersCtrl', function AnswersCtrl($scope, $stateParams, QuestionsFactory, UtilitiesFactory) {
     $scope.question = UtilitiesFactory.findById(QuestionsFactory.questions, $stateParams.questionId)
     $scope.addAnswer = function() {
-        $scope.question.answers.push({ answer_key: $scope.answer });
-        $scope.answer = null;
+        $scope.question.answers.push({ answer_key: $scope.user_answer });
+        $scope.user_answer = null;
+        console.log($scope.question.answers);
     }
 })
