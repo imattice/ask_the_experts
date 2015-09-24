ee2416560cc32de76b5390bb50ad915606ce8c2e
Jason Bethel & Ike Mattice <student@epicodus.com>
2015-09-24 12:32:30 -0700

    begin writing upvote functionality

--- controllers/AnswersController.js
+++ controllers/AnswersController.js
@@ -9,4 +9,15 @@ askTheExperts.controller('AnswersCtrl', function AnswersCtrl($scope, $stateParam
     $scope.rate = function() {
         $scope.question.answers.push({ rating_key: $scope.rating})
     }
+
+    $scope.vote = function() {
+        var vote = 0;
+        if(banana === true) {
+            vote ++;
+        } else if(poo === true) {
+            vote --;
+        }
+        $scope.question.answers.push({ vote_key: $scope.vote })
+        return vote;
+    }
 })
